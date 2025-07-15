622efff4beb321df180b53739325b552ad542ad1
Thomas Le <thomas@Thomass-MacBook-Pro.local>
2025-07-15 13:31:33 -0700

    completed input parsing

--- planner.py
+++ planner.py
@@ -3,37 +3,18 @@ import random
 import heapq
 
 
-
+# run this to test:  python3 planner.py depth-first test_worlds/sample-5x7.txt
 # Read .txt and extract it
-# planner.py uniform−cost sample −5x7 .txt
 file_path = sys.argv[2]
 with open(file_path, 'r') as f:
      lines = f.readlines()
+
+columns = int(lines[0])
+rows = int(lines[1])
+grid = []
+for line in lines [2:]:
+     clean_line = line.strip() # remove the \n
+     row_characters = list(clean_line)
+     grid.append(row_characters)
+print(grid)
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Uniform-Cost Search (UCS) - least cost path
-
-'''
-graph: a dictionary represent the graph
-start is the starting node and goal will be the goal node
-'''
-
-
-
-
-
